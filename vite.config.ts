616ca98df72da486da510f810e07ad231c475afe
Shubham <mynameshubham23@gmail.com>
2025-11-19 22:36:06 +0530

    Deploymeny fix 1

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/easynews/',
   plugins: [vue()],
+  build: {
+    outDir: 'docs',
+  },
 })
